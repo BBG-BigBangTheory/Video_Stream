26cf76ab7be676e03213480e22b9d68b597daad7
BBG-BigBangTheory <79611737+BBG-BigBangTheory@users.noreply.github.com>
2021-02-24 19:08:27 -0500

    Add files via upload

---
+++ admin/case/case.video_stream.php
@@ -0,0 +1,53 @@
+<?php
+
+/************************************************************************/
+
+/* PHP-NUKE: Advanced Content Management System                         */
+
+/* ============================================                         */
+
+/*                                                                      */
+
+/* Video Stream Module for PHP-Nuke with many features                  */
+
+/*                                                                      */
+
+/* Copyright (c) 2006 by Scott Cariss (Brady)                           */
+
+/* http://www.steezmatic-designs.com                                    */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+global $admin_file;
+
+if (!defined('ADMIN_FILE')) {
+   die ('Illegal File Access');
+}
+
+
+
+switch($op) {
+
+    case "video_stream":
+
+    include("admin/modules/video_stream.php");
+
+    break;
+
+
+
+}
+
+
+
+?>
---
+++ admin/links/links.video_stream.php
@@ -0,0 +1,45 @@
+<?php
+
+/************************************************************************/
+
+/* PHP-NUKE: Advanced Content Management System                         */
+
+/* ============================================                         */
+
+/*                                                                      */
+
+/* Video Stream Module for PHP-Nuke with many features                  */
+
+/*                                                                      */
+
+/* Copyright (c) 2006 by Scott Cariss (Brady)                           */
+
+/* http://www.steezmatic-designs.com                                    */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+global $admin_file;
+
+if (!defined('ADMIN_FILE')) {
+    die ('Illegal File Access');
+}
+
+if ($radminsuper==1) {
+
+    adminmenu("".$admin_file.".php?op=video_stream", "Video Stream", "video_stream.gif");
+
+}
+
+
+
+?>
