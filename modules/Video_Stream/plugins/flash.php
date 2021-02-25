31516e44bedcf17e52cc7c42ce70f05ce7cb7f34
BBG-BigBangTheory <79611737+BBG-BigBangTheory@users.noreply.github.com>
2021-02-24 19:10:02 -0500

    Add files via upload

---
+++ modules/Video_Stream/plugins/flash.php
@@ -0,0 +1,25 @@
+<?php
+/************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                         */
+/* ============================================                         */
+/*                                                                      */
+/* Video Stream Module for PHP-Nuke with many features                  */
+/*                                                                      */
+/* Copyright (c) 2006 by Scott Cariss (Brady)                           */
+/* http://www.steezmatic-designs.com                                    */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+if (!defined('MODULE_FILE')) 
+	{
+	   die ("You can't access this file directly...");
+	}
+// Flash suport
+$embedcode = "<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0\" width=\"".$row['width']."\" height=\"".$row['height']."\">\n";
+$embedcode .= "<param name=\"movie\" value=\"".$row['url']."\">\n";
+$embedcode .= "<param name=\"quality\" value=\"high\">\n";
+$embedcode .= "<embed src=\"".$row['url']."\" quality=\"high\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\" type=\"application/x-shockwave-flash\" width=\"".$row['width']."\" height=\"".$row['height']."\"></embed>\n";
+$embedcode .= "</object>";
+?>
---
+++ modules/Video_Stream/plugins/winamp.php
@@ -0,0 +1,21 @@
+<?php
+/************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                         */
+/* ============================================                         */
+/*                                                                      */
+/* Video Stream Module for PHP-Nuke with many features                  */
+/*                                                                      */
+/* Copyright (c) 2006 by Scott Cariss (Brady)                           */
+/* http://www.steezmatic-designs.com                                    */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+if (!defined('MODULE_FILE')) 
+	{
+	   die ("You can't access this file directly...");
+	}
+// WINAMP
+$embedcode = "<embed type=\"application/x-nsv-vp3-mp3\" width=\"".$row['width']."\" height=\"".$row['height']."\" codebase=\"http://www.nullsoft.com/nsv/embed/nsvmoz_vp3_mp3.xpi\" location=\"".$row['url']."\"></embed>";
+?>
